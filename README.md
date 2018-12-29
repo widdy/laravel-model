09f2b3c7ba16c415d84beccaea5e7be286297cd4
widdy <kingwide@foxmail.com>
2018-12-29 14:27:23 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # laravel-model
+
 a simple way to create a new Eloquent model with given table name.
+
+## Requirements
+
+- laravel >=5.5
+
+
+## Installation
+
+```sh
+$ composer require widdy/laravel-model
+```
+
+## Examples
+```
+model('users')::find(1);
+model('users')->fill(['email' => 'foo@bar.com'])->save();
+model('users', ['email' => 'foo@bar.com'])->save();
+```
---
+++ src/helpers.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * User: widdy
+ * Date: 2018/12/29
+ * Time: 14:11
+ */
+if (!function_exists('model')) {
+    /**
+     * return a new Eloquent model with given table name.
+     * we use function `eval` to generate an anonymous class(extend Illuminate\Database\Eloquent\Model),
+     * so please ensure that the table name is not from user's input which is not safe.
+     *
+     * @param $table
+     * @param array $attributes
+     * @return \Illuminate\Database\Eloquent\Model|Eloquent
+     */
+    function model(string $table, array $attributes = [])
+    {
+        return eval('return $model = new class($attributes) extends Illuminate\Database\Eloquent\Model {
+            use Illuminate\Database\Eloquent\SoftDeletes;
+            protected $guarded = [];
+			protected $table = \'' . $table . '\';	
+        };');
+    }
+}
+
+
+if (!function_exists('eloquent')) {
+    /**
+     * return a new Eloquent model with given table name.
+     * we use function `eval` to generate an anonymous class(extend Illuminate\Database\Eloquent\Model),
+     * so please ensure that the table name is not from user's input which is not safe.
+     *
+     * @param $table
+     * @param array $attributes
+     * @return \Illuminate\Database\Eloquent\Model|Eloquent
+     */
+    function eloquent(string $table, array $attributes = [])
+    {
+        return eval('return new class($attributes) extends Illuminate\Database\Eloquent\Model {         
+			protected $table = \'' . $table . '\';	
+        };');
+    }
+}
